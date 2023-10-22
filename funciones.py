22479483c0dc31e8bb9a8acdd4847bc596b282bc
AlejandraMata15 <93355429+AlejandraMata15@users.noreply.github.com>
2023-10-22 00:21:23 -0600

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,6 +1,18 @@
 import numpy as np
 from fractions import Fraction
 
+def ae(n,alpha):
+	x=alpha
+	y=n
+	while(y>0):
+		r=x%y
+		x=y
+		y=r
+		if y!=0:
+			print(str(x)+'=('+str(int(x/y))+')'+str(y)+'+'+str(x%y))
+	print('\nEl MCD de ('+str(n)+','+str(alpha)+') es = '+str(x))
+	return x
+
 def submatriz(matriz, row, col):
     return [fila[:col] + fila[col + 1:] for fila in (matriz[:row] + matriz[row + 1:])]
 
@@ -24,13 +36,23 @@ def matriz_inversa(matriz):
     for i in range(filas):
         for j in range(columnas):
             elemento = Fraction(matriz_inv[i,j]).limit_denominator(1000)
+            matriz_inv[i,j] = elemento
             print(elemento, end="\t")
         print()
+    print(matriz_inv)
+    return matriz_inv
+    
 
+def modulo(matriz):
+    print("modulo")
+    
+modulo = 27
 matriz =[[5,17,20], [9,23,3],[2,11,13]]
 det = determinante(matriz)
 print("El determinante de la matriz es: ", det)
-matriz_inv = matriz_inversa(matriz)
-
-
-
+euclides = ae(det,modulo)
+if euclides == 1:
+    matriz_inv = matriz_inversa(matriz)
+    print(matriz_inv)
+else:
+     print("La matriz no es valida, pruebe con otra")
