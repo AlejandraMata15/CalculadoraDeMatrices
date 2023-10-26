86631b67cbeec3e33b9b82982b4d119eed28f090
VillalobosLobo <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>
2023-10-26 17:23:39 -0600

    use numpy

--- funciones.py
+++ funciones.py
@@ -1,24 +1,40 @@
 import numpy as np
 import os
 
-#Mostrar matriz
-def mostrar(tam,matriz):
-	for i in range(1,(tam*tam)+1):
-		print(matriz[i-1],end='')
-		if i%tam==0:
-			print('')
-	print('')
+#Convertir un array a matriz
+def conv(matriz,tam):
+	if tam==2:
+		return [[matriz[0],matriz[1]],[matriz[2],matriz[3]]]
+	elif tam==3:
+		return [[matriz[0],matriz[1],matriz[2]],[matriz[3],matriz[4],matriz[5]],[matriz[6],matriz[7],matriz[8]]]
+	elif tam==4:
+		return [[matriz[0],matriz[1],matriz[2],matriz[3]],[matriz[4],matriz[5],matriz[6],matriz[7]],[matriz[8],matriz[9],matriz[10],matriz[11]],[matriz[12],matriz[13],matriz[14],matriz[15]]] 
 
 #Pal determinante osi osi
-def det(matriz,tam):
-	#Caso base
-	if tam==2:
-		return matriz[0]*matriz[3]-matriz[1]*matriz[2]
-	elif tam==3:
-		return matriz[0]*det(list([matriz[4],matriz[5],matriz[7],matriz[8]]),tam-1)-matriz[1]*det(list([matriz[3],matriz[5],matriz[6],matriz[8]]),tam-1)+matriz[2]*det(list([matriz[3],matriz[4],matriz[6],matriz[7]]),tam-1)
-	elif tam==4:
-		return matriz[0]*det(list([matriz[5],matriz[6],matriz[7],matriz[9],matriz[10],matriz[11],matriz[13],matriz[14],matriz[15]]),tam-1)-matriz[1]*det(list([matriz[4],matriz[6],matriz[7],matriz[8],matriz[10],matriz[11],matriz[12],matriz[14],matriz[15]]),tam-1)+matriz[2]*det(list([matriz[4],matriz[5],matriz[7],matriz[8],matriz[9],matriz[11],matriz[12],matriz[13],matriz[15]]),tam-1)-matriz[3]*det(list([matriz[4],matriz[5],matriz[6],matriz[8],matriz[9],matriz[10],matriz[12],matriz[13],matriz[14]]),tam-1)
+def det(matriz):
+	m=np.array(matriz)
+	salida=np.linalg.det(m)
+	return round(salida)
 
+#Adjunta de una matriz
+def cofactores(matrix):
+	num_rows, num_cols = matrix.shape
+	cofactors = np.zeros((num_rows, num_cols), dtype=int)
+
+	for i in range(num_rows):
+		for j in range(num_cols):
+			submatrix = np.delete(np.delete(matrix, i, axis=0), j, axis=1)
+			cofactors[i, j] = (-1) ** (i + j) * int(round(np.linalg.det(submatrix)))
+	return cofactors
+
+def adjunta(matriz):
+	matrizCof=cofactores(matriz)
+	adjunta=matrizCof.T
+	return adjunta
+
+#Tranpuesta
+def transpuesta(matriz):
+	return np.transpose(matriz)
 
 #Para saber si son coprimos su MCD debe ser 1
 def mcd(n,modulo):
@@ -30,15 +46,16 @@ def mcd(n,modulo):
 		y=r
 	return x
 
-#Obteniendo la adjunta de una matriz
-def adjunta(matriz,tam):
-	#Caso base
-	if tam==2:
-		return [matriz[3],-matriz[2],-matriz[1],matriz[0]]
-	elif tam==3:
-		return [det([matriz[4],matriz[5],matriz[7],matriz[8]],tam-1),-det([matriz[3],matriz[5],matriz[6],matriz[8]],tam-1),det([matriz[3],matriz[4],matriz[6],matriz[7]],tam-1),-det([matriz[1],matriz[2],matriz[7],matriz[8]],tam-1),det([matriz[0],matriz[2],matriz[6],matriz[8]],tam-1),-det([matriz[0],matriz[1],matriz[6],matriz[7]],tam-1),det([matriz[1],matriz[2],matriz[4],matriz[5]],tam-1),-det([matriz[0],matriz[2],matriz[3],matriz[5]],tam-1),det([matriz[0],matriz[1],matriz[3],matriz[4]],tam-1)]
-	elif tam==4:
-		return [det([matriz[5],matriz[6],matriz[7],matriz[9],matriz[10],matriz[11],matriz[13],matriz[14],matriz[15]],tam-1),-det([matriz[4],matriz[6],matriz[7],matriz[8],matriz[10],matriz[11],matriz[12],matriz[14],matriz[15]],tam-1),det([matriz[4],matriz[5],matriz[7],matriz[8],matriz[9],matriz[11],matriz[12],matriz[13],matriz[15]],tam-1),-det([matriz[4],matriz[5],matriz[6],matriz[8],matriz[9],matriz[10],matriz[12],matriz[13],matriz[14]],tam-1),-det([matriz[1],matriz[2],matriz[3],matriz[9],matriz[10],matriz[11],matriz[13],matriz[14],matriz[15]],tam-1),det([matriz[0],matriz[2],matriz[3],matriz[8],matriz[10],matriz[11],matriz[12],matriz[14],matriz[15]],tam-1),-det([matriz[0],matriz[1],matriz[3],matriz[8],matriz[9],matriz[11],matriz[12],matriz[13],matriz[15]],tam-1),det([matriz[0],matriz[1],matriz[2],matriz[8],matriz[9],matriz[10],matriz[12],matriz[13],matriz[14]],tam-1),det([matriz[1],matriz[2],matriz[3],matriz[5],matriz[6],matriz[7],matriz[13],matriz[14],matriz[15]],tam-1),-det([matriz[0],matriz[2],matriz[3],matriz[4],matriz[6],matriz[7],matriz[12],matriz[14],matriz[15]],tam-1),det([matriz[0],matriz[1],matriz[3],matriz[4],matriz[5],matriz[7],matriz[12],matriz[13],matriz[15]],tam-1),-det([matriz[0],matriz[1],matriz[2],matriz[4],matriz[5],matriz[6],matriz[12],matriz[13],matriz[14]],tam-1),-det([matriz[1],matriz[2],matriz[3],matriz[5],matriz[6],matriz[7],matriz[9],matriz[10],matriz[11]],tam-1),det([matriz[0],matriz[2],matriz[3],matriz[4],matriz[6],matriz[7],matriz[8],matriz[10],matriz[11]],tam-1),-det([matriz[0],matriz[1],matriz[3],matriz[4],matriz[5],matriz[7],matriz[8],matriz[9],matriz[11]],tam-1),det([matriz[0],matriz[1],matriz[2],matriz[4],matriz[5],matriz[6],matriz[8],matriz[9],matriz[10]],tam-1)]
+#Para mostrar la inversa
+def mostrar(matriz,det):
+	fila=len(matriz)
+	columna=len(matriz[0])
+
+	for i in range(fila):
+		print('[',end='')
+		for j in range(columna):
+			print(' '+str(matriz[i][j])+'/'+str(det)+' ',end='')
+		print(']')
 
 #Para ingresar la matriz
 def ingreMatriz(tam,mod):
@@ -48,14 +65,21 @@ def ingreMatriz(tam,mod):
 		val=int(input())
 		matriz.append(val)
 	os.system('clear')
-	mostrar(tam,matriz)
-	determinante=det(matriz,tam)
+	matriz=np.array(conv(matriz,tam))
+	print(matriz)
+	determinante=det(matriz)
 	print('Determinante de la matriz : '+str(determinante))
 	
-	print(adjunta(matriz,tam))
-	print()
 	if mcd(determinante%mod,mod)==1:
 		print('La matriz es invertible en módulo '+str(mod)+' ya que '+str(mod)+' y '+str(determinante%mod)+' son coprimos\n')
+		print('La adjunta es:')
+		matriz=adjunta(matriz)
+		print(matriz)
+		print('La transpuesta es:')
+		matriz=transpuesta(matriz)
+		print(matriz)
+		print('\nLa matriz inversa es : ')
+		mostrar(matriz,determinante)
 	else:
 		print('La matriz no es invertible en módulo '+str(mod)+' porque no son coprimos\n')
 
